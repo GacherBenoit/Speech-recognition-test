0fcb88048ba940fa5c5da2be49d074aef3202894
Benoit Gacher <benoitgacher@gmail.com>
2022-06-21 12:49:10 +0200

    runtime error fixed

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,12 +1,27 @@
 // == Import
-import reactLogo from './react-logo.svg';
 import './styles.css';
+import regeneratorRuntime from "regenerator-runtime";
+import {BrowserRouter, Route, Link, Redirect} from "react-router-dom"
+import SpeechRecognition from 'react-speech-recognition';
+import React, { useState } from 'react';
+import Home from '../Home/Home';
+import Blog from '../Blog/Blog';
+import Contact from '../Contact/contact';
+import NewBlogPost from '../NewBlogPost/NewBlogPost';
+
+
+
+
 
 // == Composant
 function App() {
   return (
     <div className="app">
-      <h1>Bienvenue</h1>
+      <BrowserRouter>
+      <div id="links">
+        <Link to="/">Home</Link>
+      </div>
+      </BrowserRouter>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import "./styles/index.scss";
 // Composants
 import App from 'src/components/App';
 
+
 // == Render
 // 1. Élément React racine (celui qui contient l'ensemble de l'app)
 //    => crée une structure d'objets imbriqués (DOM virtuel)
