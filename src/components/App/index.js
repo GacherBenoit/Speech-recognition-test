0090dd9dd617b1c98d9ac85ce3c8e87a591fdf68
Benoit Gacher <benoitgacher@gmail.com>
2022-06-21 15:25:15 +0200

    need to fix some bugs

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,8 +2,8 @@
 import './styles.css';
 // Fix generator Runtime error with import 
 import regeneratorRuntime from "regenerator-runtime";
-import {BrowserRouter, Route, Link, Redirect, Routes} from "react-router-dom"
-import SpeechRecognition from 'react-speech-recognition';
+import {BrowserRouter, Route, Link, Navigate, Routes} from "react-router-dom"
+import SpeechRecognition, {useSpeechRecognition,} from 'react-speech-recognition';
 import React, { useState } from 'react';
 import Home from '../Home/Home';
 import Blog from '../Blog/Blog';
@@ -15,7 +15,44 @@ import NewBlogPost from '../NewBlogPost/NewBlogPost';
 
 
 // == Composant
-function App() {
+function App() 
+{
+  // == commands est un tableau d'objet contenant la liste des commandes
+const commands = [
+  {
+    command: ["Go to *", "Open *"], // * signifie que le mot utilisé (exemple Go to Home) sera passé en paramètre de la callback (ici redirectPage)
+    callback: (redirectPage) => setRedirectUrl(redirectPage),
+  },
+];
+
+const {transcript} = useSpeechRecognition({commands});
+const [ redirectUrl, setRedirectUrl] = useState("");
+
+const pages = ["home", "blog", "new blog post" , "contact"];
+const urls = {
+  home: '/',
+  blog: '/blog',
+  "new blog post": "/blog/new",
+  contact: "/contact",
+};
+
+if (!SpeechRecognition.browserSupportsSpeechRecognition){
+  return null;
+}
+
+let redirect = "";
+
+if (redirectUrl)
+{
+  if (pages.includes(redirectUrl)) {
+    redirect= <Navigate to= {urls[redirectUrl]} />;
+  }
+    else {
+      redirect = <p>Ont ne peut pas trouver la page {redirectUrl}</p>;
+    }
+}
+
+
   return (
     <div className="app">
       <BrowserRouter>
@@ -32,7 +69,13 @@ function App() {
         <Route path="/contact" exact element={<Contact/>} />
         <Route path="//newBlogPost" exact element={<NewBlogPost/>} />
       </Routes>
+      {redirect}
       </BrowserRouter>
+
+      <p id="transcript"> Transcription : {transcript} </p>
+
+      <button onClick={SpeechRecognition.startListening}>Start</button>
+
     </div>
   );
 }
